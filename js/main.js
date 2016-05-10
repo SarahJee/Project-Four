7a44e67fb7f53ff4d8b5fa7cb856f2b54ce4f0bc
Sarah Jee Watson <design@sarahjee.com>
2016-05-10 08:57:28 +0800

    Trying to add Nav

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-
-
+//Get the images
+var $imgs = $('ul#imageGallery li a img'); 
 
 //Create an overlay (Lightbox) (Use $ to show $overlay is a jQuery Object)
 var $overlay = $('<div id="overlay"></div>');
-//Add the large image to the overlay
+
+//Get the large image to add to the overlay
 var $image = $('<img>');
+
 //Add catopion to the overlay
 var $caption = $('<p></p>');
 
@@ -14,8 +16,8 @@ $overlay.append($image);
 $overlay.append($caption);
 //Add overlay
 $('body').append($overlay);
-//Add caption
 
+//Add caption
 // Capture the click event on a link to an image 
 $('#imageGallery a').click(function(event) {
 	event.preventDefault();//prevent click opening image
@@ -39,9 +41,55 @@ $overlay.click(function(){
 	$overlay.hide();
 });
 
+/*=======================ADD NAVIGATION=====================//	
+//https://teamtreehouse.com/community/how-to-create-a-navigation-arrows-to-jquery-basic-course-simple-lightbox
+//Keep track of image index for prev/next
+var $imgIndex = 0;
+
+//Get the items from the gallery - add.length so it still works if more images are added
+var $galleryLength = $('imageGallery li').length;
 
 
-	
+// Add some nav buttons and giv them IDs.
+$overlay.children("div").append("<button id='btnPrev'> < </button>");
+$overlay.children("div").append("<button id='btnNext'> > </button>");
+*/
+
+
+/*
+var index = 0;
+
+function prevNext(){
+
+  var title = $('ul#imageGallery').children(index).attr('title');
+  $imgs.attr('src',imgs);
+  $overlay.show();
+  $caption.text(title);
+}
+*/
+
+$('.next').click(function(event) { //triggers when you click on 'next'
+	getNextImage(); //call the function that captures information about the next image inline to be shown
+	$image.show(); // unhide it
+});
+
+function getCurrentImage (currentImage) { //receive the image container that was clicked on
+	thisImage = $(currentImage).children(); //identify the child of the image container i.e. the image itself 
+	thisImageLocation = $(thisImage).attr("href"); //get the href and src of clicked img
+	$image.attr("src", nextImageLocation); // update overlay info to clicked img
+}
+
+function getNextImage() {
+	nextImageParent = $(thisImage).parent().next(); //get img container of next image
+	nextImage = $(nextImageParent).children(); //identify the child of the next image container
+	nextImageLocation = $(nextImage).attr("href"); //get the href and src of next img
+	$image.attr("src", nextImageParent); // update overlay info to next img
+	getCurrentImage (nextImageParent); //call the function that captures info about new current image
+}
+
+
+
+
 	
 	
 //=======================SEARCH FUNCTION=====================//	
@@ -49,7 +97,7 @@ $overlay.click(function(){
 //Set up the cache object	
 	
 (function() {
-	var $imgs = $('ul#imageGallery li a img'); //Get the images
+	
 	var $search = $('#search'); //Get the input element - the search input
 	var cache = []; //Create a cache array
 
@@ -83,7 +131,9 @@ $overlay.click(function(){
 }());
 
 
-//=======================ADD NAVIGATION=====================//	
+
+
+
 
 
 
