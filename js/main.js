b24c8bf529613b8c2ecb0dc0bf878291b957639a
Sarah Jee Watson <design@sarahjee.com>
2016-05-27 18:24:13 +0800

    Fix filter to images move inline
    And plan video…

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // Get the images
 var $imgs = $('ul#imageGallery li a img');
 // Add all images to an array  
-var imgList = ('#imageGallery a')
+var imgList = ('#imageGallery a');
 // Store current image
 var currentImg;
 
@@ -12,20 +12,22 @@ function clickPrev(event) {
     event.stopPropagation();
     currentImg = currentImg.prev();
     showOverImg();
-};
+}
 
 // Add function to prev/next arrows
 function clickNext(event) {
     event.stopPropagation();
     currentImg = currentImg.next();
     showOverImg();
-};
+}
 
 
 function showOverImg() {
         var imagehref = $(currentImg).find('a').attr('href');
         var caption = $(currentImg).find("img").attr('title');
+        var video = $(currentVideo).find("img").attr('data');
         
+        if (is data - show it) else show img
         //Update the overlay with larger image
         $("#overlay-img").attr("src", imagehref);
         //Add the caption
@@ -46,9 +48,7 @@ $('#imageGallery a').click(function(event) {
 function hideOverlay() {
     //Hide overlay	
     $('#overlay').hide();
-};
-
-
+}
 
 //=======================SEARCH FUNCTION=====================//	
 //Set up the cache object	
@@ -56,9 +56,9 @@ function hideOverlay() {
     var $search = $('#search'); //Get the input element - the search input
     var cache = []; //Create a cache array
     $imgs.each(function() { //Loop though each image in $ imgs using .each() to run an anonymous funciton on each one. 
-        cache.push({ //Add an object to the cashe array with push()
+        cache.push({ //Add an object to the cache array with push()
             element: this, // This image - objects element proprety holds ref to the img element
-            text: this.title.trim().toLowerCase() //Its title text - text property hold titlety text - remove spaces and change case. 
+            text: this.title.trim().toLowerCase() //Its title text - text property hold title text - remove spaces and change case. 
         });
     });
 
@@ -69,7 +69,9 @@ function hideOverlay() {
             if (query) { //if query has a value - some text
                 index = cacheItem.text.indexOf(query); //Use indexOf to check if search term is in text property of this object. Store a positive number if found or -1 if not found. 
             }
-            cacheItem.element.style.display = index === -1 ?
+            
+            var $image = $(cacheItem.element);// Make image a jQuery Object
+            $image.parent().parent()[0].style.display = index === -1 ? // finding the li
                 'none' : ''; //if index is -1, set display property of img to none, otherwise set display to blank string. 
         });
     }
